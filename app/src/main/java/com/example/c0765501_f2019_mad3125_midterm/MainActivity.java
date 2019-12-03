e306096a4984610f244e3a2b3577204e623f463d
komal2006 <55511982+komal2006@users.noreply.github.com>
2019-12-02 19:57:30 -0500

    [varinde] some changes

--- app/src/main/java/com/example/c0765501_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0765501_f2019_mad3125_midterm/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 dateFormat();
             }
         };
+        
         txtDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
