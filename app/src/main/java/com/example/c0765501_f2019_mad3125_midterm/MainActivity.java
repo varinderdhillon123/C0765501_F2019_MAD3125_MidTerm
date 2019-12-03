a09a8365228362315454e9f0bf8b4c1e44b42d5c
komal2006 <55511982+komal2006@users.noreply.github.com>
2019-12-02 19:57:54 -0500

    [varinde] some changes

--- app/src/main/java/com/example/c0765501_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0765501_f2019_mad3125_midterm/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sin_no = findViewById(R.id.sin_no);
